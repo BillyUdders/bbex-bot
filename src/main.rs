e17dacb598e9ca870e180e7acde54f59cefe6fca
Rhys Davies <rhys@hadrian.co>
2024-03-04 11:27:32 -0800

    Messing around with string interpolation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use axum::{routing::get, Router};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world(input_str: String) -> String {
+    format!("Hello, {}", input_str)
+}
+
+async fn goodbye_world(input_str: String) -> String {
+    format!("Goodbye, {}", input_str)
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
-    Ok(router.into())
+    Ok(Router::new()
+        .route("/hello", get(hello_world))
+        .route("/goodbye", get(goodbye_world))
+        .into())
 }
+
